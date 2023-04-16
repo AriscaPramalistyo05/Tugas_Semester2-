f6cf6aff8e55aa48f75d420010a01c7ca8ebd073
Listyo <Email>
2023-04-16 11:43:34 +0700

    first commit

---
+++ Tugas_LatihanPraktek_Semester2/Tugas1/IF_ELSE.js
@@ -0,0 +1,17 @@
+let nama = "listyo";
+let score = 85;
+let result;
+
+if (score >= 90) {
+  result = "Selamat! Anda mendapatkan nilai A.";
+} else if (score >= 80 && score <= 89) {
+  result = "Anda mendapatkan nilai B.";
+} else if (score >= 70 && score <= 79) {
+  result = "Anda mendapatkan nilai C.";
+} else if (score >= 60 && score <= 69) {
+  result = "Anda mendapatkan nilai D.";
+} else {
+  result = "Anda mendapatkan nilai E.";
+}
+
+console.log("Selamat " + nama, result);
